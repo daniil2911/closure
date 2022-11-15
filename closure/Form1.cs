a31ed86bbbc82bc74d406b9775e50a20aa578b9b
Даниил Бабушкин <nyta_210897@mail.ru>
2022-11-15 20:20:07 +0500

    Добавьте файлы проекта.

---
+++ closure/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace closure
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for (int x = 1; x < 11; x++)
+            {
+                listBox1.Items.Add(x);
+                var fn = Outer();
+                Action Outer()
+                {
+                    int X = 1;  
+                    void Inner()    
+                    {
+                        x++;       
+                        Console.WriteLine(x);
+                    }
+                    return Inner;
+                }
+            }
+        }
+    }
+}
+
